005c516114e1c42eb1444415f4d6c06e0e18984e
GulnazBakirova <gulnazbackirova@yandex.ru>
2020-10-23 16:32:34 +0300

    (пред)последние правки
    Выполнила почти все, сделала анимацию, но она почему-то не работает, хотя в браузере он пишет, что все ок

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
  const link = document.querySelector(".search-btn");
    const searchForm = document.querySelector(".search-form");
    const date = searchForm.querySelector("#search-form-date-arrival");
+   const checkin = link.querySelector("[id=search-form-date-arrival]");
+ const checkout = link.querySelector("[id=search-form-date-departure]");
+ const adults = link.querySelector("[adult-amount]");
 
   link.addEventListener("click", function(event) {
     event.preventDefault();
@@ -11,5 +14,11 @@
       searchForm.classList.add("search-form-visible");
       searchForm.classList.remove("search-form-invisible");
     };
+     if (!checkin.value || !checkout.value || !adults.value) {
+ evt.preventDefault();
+searchForm.classList.add("modal-error");
+ }
     date.focus();
   });
+
+
