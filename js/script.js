abac533509d766bbc4d858ef54faa852f655465e
GulnazBakirova <gulnazbackirova@yandex.ru>
2020-10-23 20:48:54 +0300

    (пред)последние правки 2

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@
    const checkin = link.querySelector("[id=search-form-date-arrival]");
  const checkout = link.querySelector("[id=search-form-date-departure]");
  const adults = link.querySelector("[adult-amount]");
+ const show = link.querySelector("search-form-send-btn");
+
+ var isStorageSupport = true;
+const storage = "";
+
+try {
+  storage = localStorage.getItem("checkin");
+  storage = localStorage.getItem("checkout");
+  storage = localStorage.getItem("adults");
+} catch (err) {
+  isStorageSupport = false;
+}
 
   link.addEventListener("click", function(event) {
     event.preventDefault();
@@ -14,11 +26,29 @@
       searchForm.classList.add("search-form-visible");
       searchForm.classList.remove("search-form-invisible");
     };
-     if (!checkin.value || !checkout.value || !adults.value) {
- evt.preventDefault();
-searchForm.classList.add("modal-error");
- }
     date.focus();
   });
+
+
+searchForm.addEventListener("submit", function (evt) {
+  if (!checkin.value || !checkout.value || !adults.value) {
+    evt.preventDefault();
+    searchForm.classList.remove("modal-error");
+    searchForm.offsetWidth = searchForm.offsetWidth;
+    searchForm.classList.add("modal-error");
+      } else {
+        if (isStorageSupport) {
+    localStorage.setItem("checkin", checkin.value);
+   localStorage.setItem("checkout", checkout.value);
+   localStorage.setItem("adults", adults.value);
+  }
+  }
+});
+
+
+
+
+
+
 
 
